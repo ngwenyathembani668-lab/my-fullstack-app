d89c53b2d8f830b7560c0a8b67404de4bfb477d1
ngwenyathembani668-lab <ngwenyathemabni668@gmail.com>
2026-08-06 21:21:32 +0200

    Editing my .gitignore files

---
+++ backend/src/index.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Connecting to MangoDB!
+mongoose.connect(process.env.MONGODB_URI || "")
+    .then(() => {
+        console.log("Connected to MongoDB successfully!");
+
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
+        });
+    })
+    .catch((error) => {
+        console.log("MongoDB connection failed:", error);
+    });
+
+// this is the normal routing!
+app.get('/', (req, res) => {
+    res.send('Server is running!');
+});
